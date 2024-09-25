afaf49a159588d56e6876c5def6777a3b61bec0f
Prabhjot2373kaur <152674371+Prabhjot2373kaur@users.noreply.github.com>
2024-09-24 19:59:50 -0600

    week-2 change

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function Page(){
   return (
     <div>
-      <h1>CPRG 306 assignments</h1>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="./week-2">Week-2 assignment</Link>
     </div>
 
