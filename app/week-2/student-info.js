159f6b97ebde8467b5c692899f2f4e89ed78768c
Prabhjot2373kaur <152674371+Prabhjot2373kaur@users.noreply.github.com>
2024-09-24 19:56:11 -0600

    week2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+// Importing the necessary modules
+import Link from "next/link";
+
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h2> Prabhjot kaur</h2>     
+        <Link href="https://github.com"> Github</Link>
+    </div>
+  );
+};
+
