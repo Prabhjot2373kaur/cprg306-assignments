fc995a3027c062986694d07ea3a4dc35f09400dc
Prabhjot2373kaur <152674371+Prabhjot2373kaur@users.noreply.github.com>
2024-10-15 23:25:11 -0600

    week-5

--- app/page.js
+++ app/page.js
@@ -7,7 +7,10 @@ export default function Page(){
       <li><Link href="./week-2">Week-2 assignment</Link></li>
       <li><Link href="./week-3">Week-3 assignment</Link></li>
       <li><Link href="./week-4">Week-4 assignment</Link></li>
-
+      <li><Link href="./week-5">Week-5 assignment</Link></li>
+      <li><Link href="./week-6">Week-6 assignment</Link></li>
+      <li><Link href="./week-7">Week-7 assignment</Link></li>
+      <li><Link href="./week-">Week-8 assignment</Link></li>
       </ul>
     </div>
 
